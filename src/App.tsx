9044000866cb25eb7327919dc84932606ae59472
herbig <mikeherbig@gmail.com>
2024-02-20 19:02:14 -0500

    booster crackin

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,74 @@
-import { Text, VStack, Link } from '@chakra-ui/react'
+import { Text, Button, Card, Image, Link, SimpleGrid, VStack, useToast, Skeleton, Box } from '@chakra-ui/react'
+import axios from 'axios';
+import { useState } from 'react';
 
-function App() {
+interface Booster {
+  foilIndex: number;
+  cards: Card[];
+}
+
+interface Card {
+  id: string;
+  name: string;
+}
+
+export function App() {
+  const [booster, setBooster] = useState<Booster>();
+  const [loading, setLoading] = useState(false);
+
+  const errorToast = useToast()
+  const fetchBooster = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get('https://fourcores.xyz/api/boosters?setCode=bet&count=1');
+      setBooster(response.data[0]);
+      console.log(response.data[0]);
+    } catch (error) {
+      errorToast({
+        title: 'Oops!',
+        description: "Something's up, please try again.",
+        status: 'error',
+        duration: 2000,
+        isClosable: false,
+      })
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <VStack paddingTop='10rem'>
-      <Text fontSize='xxx-large'>
-        Coming Soon
+    <VStack padding={"8rem 4rem 8rem 4rem"} spacing='1rem'>
+      <Text fontSize='xx-large'>
+        Coming Soon...
       </Text>
       <Text>
         A collection of community built <Link color='teal' isExternal href='https://sorcerytcg.com/'>Sorcery TCG</Link> software projects.
       </Text>
+      <Text>
+        But while you're here, you might as well crack a Beta booster.
+      </Text>
+      <Button onClick={fetchBooster} disabled={loading}>
+        {loading ? 'Loading...' : 'Crack Booster'}
+      </Button>
+      <SimpleGrid columns={[1,2,3,4,5]} spacing={4}>
+        {booster?.cards.map((card, index) => (
+          <Box key={card.id} position="relative">
+            <Image src={`https://sorcery-api.s3.amazonaws.com/${card.id}.png`} />
+            <Skeleton
+              hidden={index !== booster.foilIndex}
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                borderRadius: "1rem"
+              }}
+              speed={2}
+            />
+          </Box>
+        ))}
+      </SimpleGrid>
     </VStack>
   )
 }
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import { extendTheme } from '@chakra-ui/react'
+import { App } from './App'
 
 const config = {
   initialColorMode: 'dark',
